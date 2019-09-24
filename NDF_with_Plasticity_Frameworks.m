5125fd2a2b5a97d6c0d43ca67e81117c18f39d9f
jtg374 <jtg374@nyu.edu>
2019-09-24 17:15:57 +0800

    datapath

--- NDF_with_Plasticity_Frameworks.m
+++ NDF_with_Plasticity_Frameworks.m
@@ -28,8 +28,7 @@ Tmax = param.Tmax;
 
 %% Solving ODE equations
 clear textprogressbar % will cause trouble if integrate with @odetpbar while textprogressbar is in environment
-%options = odeset('RelTol',1e-3,'AbsTol',1e-5,'OutputFcn',@odetpbar); % will print progressbar
-options = odeset('RelTol',1e-3,'AbsTol',1e-5);
+options = odeset('RelTol',1e-3,'AbsTol',1e-5,'OutputFcn',@odetpbar); % will print progressbar
 disp(['Integration started at: ',datestr(now,'HH:MM:SS')])
 [t,y] = ode23(@(t,y0) NDF_with_Plasticity_Equations(t,y0,param),...
     0:dt_store:Tmax,y0,options);
@@ -48,9 +47,9 @@ clear Rt;
 clear y
 
 %% save results
-datapath = ['../../Data/WM_Plasticity/XSRule',num2str(perturbation*100),'GlobalPerturb',datestr(now,'_yyyyddmm_HH_MM')];
+datapath = ['/gpfsnyu/scratch/jtg374/WM_Plasticity/XSRule',num2str(perturbation*100),'GlobalPerturb',datestr(now,'_yyyyddmm_HH_MM')];
 mkdir(datapath)
 save([datapath,'/param.mat'],'-struct','param');
 save([datapath,'/results.mat'],'t','TDelayOff','RE','RI','MEEt');
 MEE_final = MEEt(:,:,end);
-save(['../../Data/WM_Plasticity/XSRule/FinalMEE',num2str(perturbation*100),'.mat'],'perturbation','MEE_final');
+%save(['../../Data/WM_Plasticity/XSRule/FinalMEE',num2str(perturbation*100),'.mat'],'perturbation','MEE_final');
