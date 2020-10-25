c682dc9e11f9a8217773eb28f7398253f01bf34e
jtg374Pro6 <jtg374@nyu.edu>
2020-10-25 19:08:12 +0800

    correct job script

--- submit_job.sh
+++ submit_job.sh
@@ -9,4 +9,4 @@
 
 module purge
 module load matlab/2018a
-matlab -nodisplay -r "NDF_with_Plasticity_Frameworks($1,$2,$3);quit"
+matlab -nodisplay -r "NDF_with_Plasticity_Frameworks(0.95,0,1e-7,5000);quit"
