26347860cf454b48261bcc5b45aa620e37ccb7c5
jtg374 <jtg374@nyu.edu>
2020-11-06 09:13:57 +0800

    saturation nonlinearity

--- NDF_with_Plasticity_Parameters.m
+++ NDF_with_Plasticity_Parameters.m
@@ -61,8 +61,8 @@ function param = NDF_with_Plasticity_Parameters(a,lr,nTrialMax)
     qI = @(x) maxfI*(x-thI).^NI./(sigI^NI+(x-thI).^NI).*(x>thI);
 %     param.qE = qE;
 %     param.qI = qI;
-    param.qE = @(x) x.*(x>0);
-    param.qI = @(x) x.*(x>0);
+    param.qE = @(x) x.*(x>0) - (x-maxfE).*(x>maxfE);
+    param.qI = @(x) x.*(x>0) - (x-maxfI).*(x>maxfI);
     %% Perturbations
 %     a = 0.9;
     % %sharp local perturbation 
