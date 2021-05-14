266a41c40d48b80feee4908af21a0dde8501c89e
jtg374 <jtg374@nyu.edu>
2021-05-14 09:39:26 +0800

    update continue

--- NDF_with_Plasticity_continue.m
+++ NDF_with_Plasticity_continue.m
@@ -36,11 +36,9 @@ param.pNp = [param.pNp(:,1);pNp];
 
 load([datapath,'results.mat']);
 %% unpack Connectivity profile 
-MEE = MEEt(:,:,end);
-g = g_readout(:,end);
-MEI = param.MEI;
-MIE = param.MIE;
-MII = param.MII;
+MEI = MEIt(:,:,end);
+% g = g_readout(:,end);
+
 
 %% pack initial values
 nx = param.N;
@@ -48,8 +46,8 @@ np = param.np;
 y0 = [0;              % Stimlus Current Strength
       0;              % Wipe Current Strength
       zeros(6*nx*np,1);  % 6*N state variables
-      reshape(MEE,nx*nx,1); % E to E Connection Strength
-      g % gains
+      reshape(MEI,nx*nx,1) % E to E Connection Strength
+      ones(nx,1) % gains
       ]; 
 
 %% load timings
@@ -58,21 +56,17 @@ TrialOn = param.TrialOn;
 T_on = param.TStimOn(1);
 TrialEnd = param.TDelayOff;
 dt_store = param.dt_store;
-%% load plasticity
-lrD = param.LearningRateDifferential;    
-lrH = param.LearningRateHomeostatic;
-r_target = param.r_target;
 
 
 %% Solving ODE equations
 options = odeset('RelTol',1e-3,'AbsTol',1e-5); 
 disp(['Integration started at: ',datestr(now,'HH:MM:SS')])
-new.MEEt = zeros(nx,nx,nTrialAdd);
+new.MEIt = zeros(nx,nx,nTrialAdd);
 new.RE_readout = zeros(nx,np,nTrialAdd);
-new.g_readout = zeros(nx,nTrialAdd);
-MEEt = cat(3,MEEt,new.MEEt);
+% new.g_readout = zeros(nx,nTrialAdd);
+MEIt = cat(3,MEIt,new.MEIt);
 RE_readout = cat(3,RE_readout,new.RE_readout);
-g_readout = cat(2,g_readout,new.g_readout);
+% g_readout = cat(2,g_readout,new.g_readout);
 clear new
 for iTrial=(nTrialOld+1):param.nTrial
     %% solve current batch
@@ -82,15 +76,15 @@ for iTrial=(nTrialOld+1):param.nTrial
     nt = length(t);
     gt = y(:,nx*nx+nx*np*6+3:end)';g=gt(:,end);
     Mt = y(:,nx*np*6+3:nx*nx+nx*np*6+2);Mt = reshape(Mt,nt,nx,nx);
-    MEE = Mt(end,:,:); MEE = squeeze(MEE);
+    MEI = Mt(end,:,:); MEI = squeeze(MEI);
     Rt = y(:,3:nx*np*6+2);Rt = reshape(Rt,nt,nx,np,6);Rt = permute(Rt,[2,3,1,4]);
     RE = Rt(:,:,:,1);RI = Rt(:,:,:,2);SEE = Rt(:,:,:,3);SIE = Rt(:,:,:,4);SEI = Rt(:,:,:,5);SII = Rt(:,:,:,6); 
     clear Rt;
-    MEEt(:,:,iTrial) = MEE;    
+    MEIt(:,:,iTrial) = MEI;    
     %% plot and save
     addpath('/gpfsnyu/home/jtg374/MATLAB/CubeHelix') 
-    if mod(iTrial,100)==0 | ismember(iTrial,[1,2,5,10,20,50])
-        save([datapath,'/FullData/results_' num2str(iTrial) '.mat'],'t','RE','RI','gt');
+    if mod(iTrial,100)==0 
+        save([datapath,'/FullData/results_' num2str(iTrial) '.mat'],'t','RE','RI');
         h2=figure; %imagesc([RE RE1])
         imagesc(squeeze(RE(:,param.pNp(iTrial),:)),[0 50]);
         ylabel('neuron')
@@ -105,31 +99,29 @@ for iTrial=(nTrialOld+1):param.nTrial
         colorbar
         saveas(h3,[datapath,'/ActFigures/RE_X_' num2str(iTrial) '.jpg'])
         h4=figure;
-        imagesc(diag(g)*MEE,[0 10])
-        xlabel('pre-syn')
-        xlabel('post-syn')
-        colormap(cubehelix)
-        colorbar
-        saveas(h4,[datapath,'/ActFigures/g-MEE_' num2str(iTrial) '.jpg'])
+        % imagesc(diag(g)*MEE,[0 10])
+        % xlabel('pre-syn')
+        % xlabel('post-syn')
+        % colormap(cubehelix)
+        % colorbar
+        % saveas(h4,[datapath,'/ActFigures/g-MEE_' num2str(iTrial) '.jpg'])
     end
     disp([num2str(iTrial) ' trials completed at: ',datestr(now,'HH:MM:SS')])
     RE_readout(:,:,iTrial) = RE(:,:,end);
-    g_readout(:,iTrial) = g;
+    % g_readout(:,iTrial) = g;
     %% update to next batch
     y0 = [  0;              % Stimlus Current Strength
             0;              % Wipe Current Strength
             zeros(6*nx*np,1);  % 6*N state variables
-            reshape(MEE,nx*nx,1); % E to E Connection Strength
-            g %gain
-            ]; 
+            reshape(MEI,nx*nx,1) % E to E Connection Strength
+            g]; 
 end
 disp(['Integration ended at:   ',datestr(now,'HH:MM:SS')])
 
 %% save results
 disp(datapath)
-save([datapath,'/results.mat'],'RE_readout','MEEt','-v7.3','g_readout');
+save([datapath,'/results.mat'],'RE_readout','MEIt','-v7.3');
 save([datapath,'/param.mat'],'-struct','param');
 saveas(h3,[datapath,'/RE_X_' num2str(iTrial) '.jpg'])
 saveas(h2,[datapath,'/RE_T_' num2str(iTrial) '.jpg'])
-saveas(h3,[datapath,'/RE_X_' num2str(iTrial) '.jpg'])
-saveas(h4,[datapath,'/g-MEE_' num2str(iTrial) '.jpg'])
+% saveas(h4,[datapath,'/g-MEE_' num2str(iTrial) '.jpg'])
